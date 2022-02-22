976a41105f7c004583e9072c595daab800d84b7b
James Murphy <jamesm8228@gmail.com>
2022-02-21 19:03:28 -0500

    Made changes to redirect the shortURL to the longURL location

--- express_server.js
+++ express_server.js
@@ -16,7 +16,7 @@ const urlDatabase = {
 
 
 function generateRandomString() {
-
+  return Math.random().toString(36).substr(2, 6);;
 }
 
 
@@ -33,26 +33,26 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// app.get("/urls", (req, res) => {
-//   const templateVars = { urls: urlDatabase };
-//   res.render("urls_index", templateVars);
-// });
-
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 
-
-
-
-
-
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString()
+  urlDatabase[shortURL] = req.body.longURL;
+  let redirectURL = `/urls/${shortURL}`;
+  res.redirect(redirectURL);        
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
