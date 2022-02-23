26f9e9461b935a4a195b73765487ab4d5fd8caf3
James Murphy <jamesm8228@gmail.com>
2022-02-23 13:34:46 -0500

    Made changes to include a registration GET method

--- express_server.js
+++ express_server.js
@@ -44,6 +44,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = { username: req.cookies["username"]}
+  res.render('urls_registration', templateVars);
+});
 
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString()
